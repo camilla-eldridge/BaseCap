8d7658e6390c7c9876b550a13cf9cc08670d04f2
Camilla Eldridge <camilla.jl.eldridge@gmail.com>
2024-05-17 21:25:48 +0100

    added f strings, main and type annotations

--- BaseCap.py
+++ BaseCap.py
@@ -4,358 +4,208 @@
 @author: camilla eldridge
 """
 
-''' Trims low qc bases, primers(if found) and output trimmed sequence and it's '.qual file '''
+''' Trims low qc bases, primers(if found) and output trimmed sequence and its '.qual file '''
 
 import regex
 import os
 import sys
-
-#import time
-#start_time = time.time()
+from typing import List, Tuple, Optional
 
 
-
-def reverse_comp(seq):
+def reverse_comp(seq: str) -> str:
     """ Return reverse complement """
     result = ""
-    seq=seq.lower()
-    comp=seq.replace("a", "T").replace("t","A").replace("c","G").replace("g","C").lower()
-    result="".join(comp[::-1])
+    seq = seq.lower()
+    comp = seq.replace("a", "T").replace("t", "A").replace("c", "G").replace("g", "C").lower()
+    result = "".join(comp[::-1])
     return result
 
 
-
-def find_variable_seq(sequence, primer, error_n):
-    """ Return variable string match allowing n errors (del &/or subst"""
-    p=str("(" + primer.lower() + ")" + "{e<=" + str(error_n) + "}")
-    q=regex.search(p, sequence.lower())
+def find_variable_seq(sequence: str, primer: str, error_n: int) -> Optional[regex.Match]:
+    """ Return variable string match allowing n errors (del &/or subst) """
+    p = str("(" + primer.lower() + ")" + "{e<=" + str(error_n) + "}")
+    q = regex.search(p, sequence.lower())
     return q
 
 
-    
-def index_phd(phd):
+def index_phd(phd: str) -> str:
     """ Index phd file - adds column of base pos """
     indexed = ""
-    phd=filter(None, phd.split("\n"))
-    
+    phd = filter(None, phd.split("\n"))
+
     for x, value in enumerate(phd, 1):
-        indexed=indexed + str(x) + " " + str(value) + "\n"
-    
+        indexed = indexed + str(x) + " " + str(value) + "\n"
+
     return indexed
 
 
-
-
-
-class Main(object): 
-    
+class Main(object):
     """   trim low quality bases and primers from .Phd file, output qual and trimmed sequence """
-    
-    def __init__(self, phd_file, ID, primers, threshold): 
-    
-        
+
+    def __init__(self, phd_file: str, ID: str, primers: str, threshold: int) -> None:
         self.phd_file = phd_file
         self.ID = ID
         self.primers = primers
         self.threshold = threshold
-        
-    
-    
-    def phd_to_sequence(self):
-        
+        self.sequence: str = ""
+        self.trim_pos: str = ""
+        self.check: int = 1
+        self.trim_start: int = 0
+        self.trim_stop: int = 0
+        self.start: int = 0
+        self.stop: int = 0
+        self.primer_pos: Optional[regex.Match] = None
+        self.search: Optional[regex.Match] = None
+        self.primz: List[str] = []
+        self.trimmed_seq: str = ""
+        self.trimmed_phd: List[str] = []
+        self.seq_cap: str = ""
+        self.scores: str = ""
+        self.phd2: str = ""
+
+    def phd_to_sequence(self) -> str:
         """ Get sequence from phd_file """
-        
-        self.sequence = ""
-        
         with open(self.phd_file) as self.phd1:
-            self.phd2=self.phd1.read()
-            self.phd=index_phd(self.phd2.split("BEGIN_DNA")[1].split("END_DNA")[0].rstrip())
-            self.sequence=self.sequence + "".join(self.phd.split()[1::4]) 
-            
+            self.phd2 = self.phd1.read()
+            self.phd = index_phd(self.phd2.split("BEGIN_DNA")[1].split("END_DNA")[0].rstrip())
+            self.sequence = self.sequence + "".join(self.phd.split()[1::4])
         return self.sequence
-    
-    
-    
-    def trim_pos_ttuner(self): 
-        
-      """ Find trim positions from base caller ttuner """
-      
-      self.trim_pos=""
-      
-      for line in self.phd2.split("\n"):  
+
+    def trim_pos_ttuner(self) -> Tuple[int, int, int]:
+        """ Find trim positions from base caller ttuner """
+        self.trim_pos = ""
+
+        for line in self.phd2.split("\n"):
             if "TRIM:" in line:
                 self.trim_pos = self.trim_pos + line
-                
-      self.trim_pos=self.trim_pos.split()[1:3]
-      
-      self.trim_start = int(self.trim_pos[0])
-      self.trim_stop = int(self.trim_pos[1])
-      
-      
-      ''' if both are <0 no trimming needed '''
-     
-      if ( self.trim_stop < 0 ) and ( self.trim_start < 0 ):
+
+        self.trim_pos = self.trim_pos.split()[1:3]
+
+        self.trim_start = int(self.trim_pos[0])
+        self.trim_stop = int(self.trim_pos[1])
+
+        if self.trim_stop < 0 and self.trim_start < 0:
             self.check = 0
-      else:
-            self.check = 1      
-    
-      return self.trim_start, self.trim_stop, self.check
-          
-        
-    
-    def trim_check(self):
-        
-        ''' See if trimming is recommended by ttuner 
-        we dont know which (if any ) primer will hit so need to keep 
-        trim pos as 0 or len(sequence) even if no trimming required '''
-        
-        
-        if self.trim_start < 0:
-                self.trim_start = 0
         else:
-                self.trim_start = self.trim_start
-                
+            self.check = 1
+
+        return self.trim_start, self.trim_stop, self.check
+
+    def trim_check(self) -> Tuple[int, int]:
+        """ See if trimming is recommended by ttuner """
+        if self.trim_start < 0:
+            self.trim_start = 0
         if self.trim_stop < 0:
             self.trim_stop = int(len(self.sequence))
-        else:
-            self.trim_stop = self.trim_stop
-        
         return self.trim_start, self.trim_stop
-        
-    
-    
-    def reverse_primers(self):  
-        
-        """ reverse complement both primers """
-        
-        self.primz=[]
+
+    def reverse_primers(self) -> List[str]:
+        """ Reverse complement both primers """
         with open(self.primers) as primerz:
-            
-                primerz=primerz.read().split("\n")[1::2] #list of primers
-                
-                """ revcomp each primer and make new list """
-                self.primz.append(primerz)
-                self.primz.append([reverse_comp(prim_seq) for prim_seq in primerz])
-        
-        self.primz=[a for b in self.primz for a in b]
-        
+            primer_list = primerz.read().split("\n")[1::2]
+            self.primz = primer_list + [reverse_comp(prim_seq) for prim_seq in primer_list]
         return self.primz
-                          
-    
-    
-    def primer_search(self):
-        
-        ''' Search for each primer (forward and reverse) - expecting one hit '''
-        
+
+    def primer_search(self) -> Optional[regex.Match]:
+        """ Search for each primer (forward and reverse) - expecting one hit """
         for p in self.primz:
-            p=str(p)
-            self.search=find_variable_seq(self.sequence,p,3)
-            if self.search == None:
-                self.primer_pos = None
-            else:
+            self.search = find_variable_seq(self.sequence, p, 3)
+            if self.search is not None:
                 self.primer_pos = self.search
-                
-                return self.primer_pos, self.search
-    
+                return self.primer_pos
+        return None
 
-
-    def locate_primer(self):
-        ''' locate which primer hit (assuming orient not known 
-        before hand) '''
-        
-        midpoint=int(len(self.sequence))/2
-        
-        ''' primer pos: e.g if the position is at the right end
-        then then primer hit to end and start is suggested at 0'''
-    
-    
-        if  self.primer_pos.span()[0] > midpoint:
+    def locate_primer(self) -> Tuple[int, int]:
+        """ Locate which primer hit (assuming orient not known beforehand) """
+        midpoint = int(len(self.sequence)) // 2
+        if self.primer_pos.span()[0] > midpoint:
             self.primer_start = 0
             self.primer_stop = int(self.primer_pos.span()[0])
-                
-        elif self.primer_pos.span()[0] < midpoint:
-            self.primer_start=int(self.primer_pos.span()[1])
+        else:
+            self.primer_start = int(self.primer_pos.span()[1])
             self.primer_stop = int(len(self.sequence))
-            
         return self.primer_start, self.primer_stop
-        
 
-    
-    def compare_trim(self):
-        
-        ''' compare primer and trim positions '''
-        ''' we have trim_start and trim_stop, also maybe primer_pos...'''
-        
-        ''' if trim pos is smaller than primer pos, trim by primer pos etc '''
-        
-        if self.trim_start < self.primer_start:
-            self.start = int(self.primer_start)
-        else:
-            self.start = self.trim_start
-        
-        if  self.trim_stop < self.primer_stop:   
-            self.stop = self.trim_stop
-        else:
-            self.stop = int(self.primer_stop)
-
+    def compare_trim(self) -> Tuple[int, int]:
+        """ Compare primer and trim positions """
+        self.start = max(self.trim_start, self.primer_start)
+        self.stop = min(self.trim_stop, self.primer_stop)
         return self.start, self.stop
-            
-    
-    
-    
-    def trim_phd_and_seq(self):
-        ''' trim phd and sequence'''
-        
+
+    def trim_phd_and_seq(self) -> Tuple[str, List[str]]:
+        """ Trim phd and sequence """
         self.trimmed_seq = self.sequence[int(self.start):int(self.stop)]
-        
-        self.trimmed_phd=self.phd.split("\n")[int(self.start):int(self.stop)]
-
+        self.trimmed_phd = self.phd.split("\n")[int(self.start):int(self.stop)]
         return self.trimmed_seq, self.trimmed_phd
-    
-    
 
-        
-    def cap_the_bases(self):
-        
-        ''' capitalise the low scoring bases
-        and get scores for qual
-        '''
-        
-        ''' trimmed and not trimmed have a different structure 
-        make sure to remove empty values in list'''
-        
+    def cap_the_bases(self) -> Tuple[str, str]:
+        """ Capitalise the low scoring bases and get scores for qual """
         self.trimmed_phd2 = [ele for ele in self.trimmed_phd if ele.strip()]
-               
         self.seq_cap = ""
         self.scores = ""
-        
-        
+
         for j in self.trimmed_phd2:
-            
-            self.scores = self.scores + j.split()[2] + " "
-            
-            if int((j.split()[2])) < int(self.threshold):
-                self.seq_cap = self.seq_cap + str(j.split()[1]).upper()
-            else:
-                self.seq_cap = self.seq_cap + str(j.split()[1]).lower()
-                
-        self.seq_cap = "".join(self.seq_cap)
-        
-    
+            parts = j.split()
+            if len(parts) >= 3:
+                score = int(parts[2])
+                base = parts[1]
+                self.scores += f"{score} "
+                self.seq_cap += base.upper() if score < self.threshold else base.lower()
         return self.seq_cap, self.scores
 
-       
-       
-        
-    def write_out(self):
-        
-        ''' write out trimmed and capped sequence '''
-        
-        self.qual_file=open(str(self.ID) + ".qual", "w")
-        self.qual_file.write(">" + str(self.ID) + "\n" + self.scores)
-        self.qual_file.close()
-        
-        self.seq_file=open(str(self.ID) + ".fasta", "w")
-        self.seq_file.write(">" + str(self.ID) + "\n" + self.seq_cap)
-        self.seq_file.close()
-        
-        return self.seq_file, self.qual_file
-    
-    
-    
-    def call_methods(self):
-        
-        ''' to call initial methods ''' 
-        
-        methods1= ["phd_to_sequence", "trim_pos_ttuner", "reverse_primers" ,"primer_search", "trim_check"]
+    def write_out(self) -> None:
+        """ Write out trimmed and capped sequence """
+        with open(f"{self.ID}.qual", "w") as qual_file:
+            qual_file.write(f">{self.ID}\n{self.scores}")
+        with open(f"{self.ID}.fasta", "w") as seq_file:
+            seq_file.write(f">{self.ID}\n{self.seq_cap}")
+
+    def call_methods(self) -> str:
+        """ Call initial methods """
+        methods1 = ["phd_to_sequence", "trim_pos_ttuner", "reverse_primers", "primer_search", "trim_check"]
         for method in methods1:
-                getattr(self, method)()
-            
-        ''' if no primers found 
-        miss out compare trim function'''        
-        
-        if self.primer_pos == None:
-            self.start = self.trim_start
-            self.stop = self.trim_stop
+            getattr(self, method)()
+
+        if self.primer_pos is None:
+            self.start, self.stop = self.trim_start, self.trim_stop
         else:
-            ''' optional methods if primers found'''
             self.locate_primer()
             self.compare_trim()
 
-        
         if self.check == 0:
             self.trimmed_seq = self.sequence
             self.trimmed_phd = self.phd.split("\n")
-            
-            
         else:
             self.trim_phd_and_seq()
 
-        ''' run final methods '''
         self.cap_the_bases()
         self.write_out()
-        
-        
-        ''' get summary info,print out and save '''
-        
-        if self.search == None:
-            self.search_out = None 
-        else:
-            self.search = self.primer_pos.group()
-        
-        ''' if trimmed_seq variable exists, get len of trimmed sequence '''    
-        if self.trimmed_seq in locals():
-            
-            self.if_trim = len(self.sequence)
-        else:
-            self.if_trim = len(self.trimmed_seq)
-        
-        
-        
-        
-        self.summary=""
-        
-        var_list=[self.ID, self.check, self.start, self.stop, len(self.sequence), self.if_trim , self.trim_start, self.trim_stop, self.search]
-        
-        for variable in var_list:
-            self.summary=self.summary + str(variable) + ","
-            
-        ''' make output in csv format, remove last comma'''
-        
-        return self.summary[:-1]
+
+        search_output = self.search.group() if self.search else "None"
+        trimmed_length = len(self.trimmed_seq) if self.check else len(self.sequence)
+
+        summary_vars = [
+            self.ID, self.check, self.start, self.stop,
+            len(self.sequence), trimmed_length,
+            self.trim_start, self.trim_stop, search_output
+        ]
+        summary = ",".join(map(str, summary_vars))
+        return summary
 
 
+if __name__ == "__main__":
+    Directory: str = sys.argv[1]
+    Primers: str = sys.argv[2]
+    Threshold: int = int(sys.argv[3])
 
+    header_list: List[str] = ["ID", "trimmed(0/1)", "trim_start", "trim_stop", "read_len", "trimmed_read_len", "primer_hit"]
 
-''' Input '''
-Directory=sys.argv[1] 
-Primers= sys.argv[2]
-Threshold=sys.argv[3]
+    print(",".join(header_list))
 
-header_list=["ID", "trimmed(0/1)" ,"trim_start", "trim_stop", "read_len", "trimmed_read_len", "primer_hit"]
+    for filename in os.listdir(Directory):
+        f = os.path.join(Directory, filename)
+        ID = str(filename.split(".")[0])
 
-print(",".join(header_list))
-           
-for filename in os.listdir(Directory):
-    
-    ''' iterate over all phd.1 files in dir'''
-    
-    f = os.path.join(Directory, filename)
-    ID = str(filename.split(".")[0])
-    
-    if filename.endswith(".phd.1"):
-        Z=Main(f, ID, Primers, Threshold)
-        print(Z.call_methods())
-
-
-
-#elapsed_time = time.time() - start_time
-
-# test 
-#Directory="phd_files/"#sys.argv[1] 
-#Primers= "test_primers.txt"#sys.argv[2]
-#Threshold=25 #sys.argv[3]
-
-        
-        
-
+        if filename.endswith(".phd.1"):
+            Z = Main(f, ID, Primers, Threshold)
+            print(Z.call_methods())
